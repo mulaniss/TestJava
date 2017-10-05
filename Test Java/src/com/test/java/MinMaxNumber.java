58a02ddbc03784b2f3fcac74fa2393cf7a13821c
Dell <Dell@192.168.0.4>
2017-10-05 23:55:33 +0530

    Added MinMax program

--- Test Java/src/com/test/java/MinMaxNumber.java
+++ Test Java/src/com/test/java/MinMaxNumber.java
@@ -3,7 +3,35 @@ package com.test.java;
 public class MinMaxNumber {
 
 	public static void main(String[] args) {
-		
+
+		int[] num = { 12, 54, 7, 82, 22, 71, 1, 99 };
+
+		int count = num.length;
+
+		int MaxNum = num[0];
+		int MinNum = num[0];
+
+		for (int i = 0; i < count; i++) {
+
+			if (num[i] > MaxNum) {
+				MaxNum = num[i];
+
+			}
+
+		}
+
+		System.out.println("Tha Max Number is " + MaxNum);
+
+		for (int i = 0; i < count; i++) {
+
+			if (num[i] < MinNum) {
+				MinNum = num[i];
+
+			}
+
+		}
+
+		System.out.println("Tha Min Number is " + MinNum);
 
 	}
 
